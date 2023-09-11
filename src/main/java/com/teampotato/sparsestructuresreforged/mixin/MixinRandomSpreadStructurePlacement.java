e5e6f060b19af55513ed35c771e0cc0b76714912
Kasualix <670067575@qq.com>
2023-09-11 12:08:10 +0800

    1192

---
+++ src/main/java/com/teampotato/sparsestructuresreforged/mixin/MixinRandomSpreadStructurePlacement.java
@@ -0,0 +1,28 @@
+package com.teampotato.sparsestructuresreforged.mixin;
+
+import com.teampotato.sparsestructuresreforged.SparseStructuresReforged;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
+import net.minecraft.world.level.levelgen.structure.placement.StructurePlacement;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+
+@Mixin(RandomSpreadStructurePlacement.class)
+public abstract class MixinRandomSpreadStructurePlacement {
+    @Mutable @Shadow @Final private int spacing;
+    @Mutable @Shadow @Final private int separation;
+
+    @Inject(method = "<init>(Lnet/minecraft/core/Vec3i;Lnet/minecraft/world/level/levelgen/structure/placement/StructurePlacement$FrequencyReductionMethod;FILjava/util/Optional;IILnet/minecraft/world/level/levelgen/structure/placement/RandomSpreadType;)V", at = @At("TAIL"))
+    private void onInit(Vec3i p_227000_, StructurePlacement.FrequencyReductionMethod p_227001_, float p_227002_, int p_227003_, Optional p_227004_, int p_227005_, int p_227006_, RandomSpreadType p_227007_, CallbackInfo ci) {
+        this.spacing = (int) ((1 + SparseStructuresReforged.extraSpacingPercentage.get()) * p_227005_);
+        this.separation = (int) ((1 + SparseStructuresReforged.extraSeparationPercentage.get()) * p_227006_);
+    }
+}
