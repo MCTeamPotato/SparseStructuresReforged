5010f32baed240e298cab351a025bbe2d43e0d65
Kasualix <670067575@qq.com>
2023-09-17 09:51:50 +0800

    fuck you stupid FG. Switch to Architectury setup. Fix https://github.com/MCTeamPotato/Kasualix-Issue-Tracker/issues/63

--- src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
+++ src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
@@ -1,7 +1,7 @@
 package com.teampotato.sparsestructuresreforged.mixin;
 
 import com.teampotato.sparsestructuresreforged.SparseStructuresReforged;
-import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraft.world.level.levelgen.feature.configurations.StructureFeatureConfiguration;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -10,13 +10,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(StructureSeparationSettings.class)
-public abstract class MixinStructureSeparationSettings {
+@Mixin(StructureFeatureConfiguration.class)
+public abstract class StructureFeatureConfigurationMixin {
     @Mutable @Shadow @Final private int spacing;
+
     @Mutable @Shadow @Final private int separation;
 
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void onInit(int spacing, int separation, int salt, CallbackInfo ci) {
+    private void onInit(int i, int j, int k, CallbackInfo ci) {
         this.spacing = (int) ((1 + SparseStructuresReforged.extraSpacingPercentage.get()) * spacing);
         this.separation = (int) ((1 + SparseStructuresReforged.extraSeparationPercentage.get()) * separation);
     }
