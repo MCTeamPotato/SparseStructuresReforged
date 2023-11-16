df4532de3da704ae3f0e3b7ae2e455747e26a12c
Kasualix <670067575@qq.com>
2023-11-16 15:15:19 +0800

    Update StructureFeatureConfigurationMixin.java

--- src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
+++ src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
@@ -19,6 +19,6 @@ public abstract class StructureFeatureConfigurationMixin {
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onInit(int i, int j, int k, CallbackInfo ci) {
         this.spacing = Math.min((int) ((1 + SSRMixinPlugin.extraSpacingPercentage) * spacing), 4095);
-        this.separation = Math.min((int) ((1 + SSRMixinPlugin.extraSeparationPercentage) * separation), 4095);
+        this.separation = Math.min((int) ((1 + SSRMixinPlugin.extraSeparationPercentage) * separation), 4094);
     }
 }
