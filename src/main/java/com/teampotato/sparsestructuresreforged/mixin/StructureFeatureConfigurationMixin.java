218447ab85ae1f397fa7a004df2ed293e9f6261d
Kasualix <670067575@qq.com>
2024-01-07 17:38:49 +0800

    1182

--- src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
+++ src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
@@ -1,7 +1,11 @@
 package com.teampotato.sparsestructuresreforged.mixin;
 
-import net.minecraft.world.level.levelgen.feature.configurations.StructureFeatureConfiguration;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,13 +14,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static com.teampotato.sparsestructuresreforged.SSRMixinPlugin.extraSeparationPercentage;
 import static com.teampotato.sparsestructuresreforged.SSRMixinPlugin.extraSpacingPercentage;
 
-@Mixin(StructureFeatureConfiguration.class)
+@Mixin(RandomSpreadStructurePlacement.class)
 public abstract class StructureFeatureConfigurationMixin {
-    @Shadow public int spacing;
-    @Shadow public int separation;
+    @Mutable
+    @Final
+    @Shadow
+    private int spacing;
+    @Mutable
+    @Final
+    @Shadow
+    private int separation;
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(int spacing, int separation, int salt, CallbackInfo ci) {
+    @Inject(method = "<init>(IILnet/minecraft/world/level/levelgen/structure/placement/RandomSpreadType;ILnet/minecraft/core/Vec3i;)V", at = @At("RETURN"))
+    private void onInit(int spacing, int separation, RandomSpreadType f_204975_, int f_204976_, Vec3i f_204977_, CallbackInfo ci) {
         double newSpacing = (extraSpacingPercentage + 1.00) * spacing;
         double newSeparation = (extraSeparationPercentage + 1.00) * separation;
         if (newSpacing > 4095) newSpacing = 4095;
