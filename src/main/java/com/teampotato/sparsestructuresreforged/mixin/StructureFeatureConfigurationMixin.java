ddb0401b30ce98afc51367c15f115e05334a9de8
Kasualix <670067575@qq.com>
2024-01-07 17:42:56 +0800

    1192

--- src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
+++ src/main/java/com/teampotato/sparsestructuresreforged/mixin/StructureFeatureConfigurationMixin.java
@@ -3,6 +3,7 @@ package com.teampotato.sparsestructuresreforged.mixin;
 import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
 import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
+import net.minecraft.world.level.levelgen.structure.placement.StructurePlacement;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -10,6 +11,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 import static com.teampotato.sparsestructuresreforged.SSRMixinPlugin.extraSeparationPercentage;
 import static com.teampotato.sparsestructuresreforged.SSRMixinPlugin.extraSpacingPercentage;
@@ -25,8 +28,8 @@ public abstract class StructureFeatureConfigurationMixin {
     @Shadow
     private int separation;
 
-    @Inject(method = "<init>(IILnet/minecraft/world/level/levelgen/structure/placement/RandomSpreadType;ILnet/minecraft/core/Vec3i;)V", at = @At("RETURN"))
-    private void onInit(int spacing, int separation, RandomSpreadType f_204975_, int f_204976_, Vec3i f_204977_, CallbackInfo ci) {
+    @Inject(method = "<init>(Lnet/minecraft/core/Vec3i;Lnet/minecraft/world/level/levelgen/structure/placement/StructurePlacement$FrequencyReductionMethod;FILjava/util/Optional;IILnet/minecraft/world/level/levelgen/structure/placement/RandomSpreadType;)V", at = @At("RETURN"))
+    private void onInit(Vec3i arg, StructurePlacement.FrequencyReductionMethod arg2, float f, int i, Optional optional, int spacing, int separation, RandomSpreadType arg3, CallbackInfo ci) {
         double newSpacing = (extraSpacingPercentage + 1.00) * spacing;
         double newSeparation = (extraSeparationPercentage + 1.00) * separation;
         if (newSpacing > 4095) newSpacing = 4095;
